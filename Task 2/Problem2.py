8f494d8cea652f4638da71ce23de65f2c52ab284
TANzim0009 <140909012+TANzim0009@users.noreply.github.com>
2026-06-09 16:05:09 +0600

    Implement logic to find most common patterns
    Fix the logic to find the most common patterns in the text.

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,15 @@
+text = input().strip()
+k = int(input())
 
+patterns = []
+for i in range (len(text)- k+1):
+  patterns.append(text[i:i+k])
+  max_count = 0
+  for p in patterns:
+    if patterns.count(p) > max_count:
+      max_count = patterns.count(p)
+result = []
+for p in patterns:
+  if patterns.count(p) == max_count and p not in result:
+    result.append(p)
+print(*result)
